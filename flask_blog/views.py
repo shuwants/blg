f001fc6ab811f53d4eea6b97777f18c87a06c638
Shuji Kato <shuwants@gmail.com>
2021-03-13 14:27:49 +0900

    Setting up Template with MTV framework

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,5 +1,9 @@
-from flask_blog import app
+from flask import request, redirect, url_for, render_template, flash, session # 後使用も含め必要パッケージインストール
+from flask_blog import app # __init__.pyで作成したappをインポート
+
 
 @app.route('/')
 def show_entries():
-	return "Hello World!"
+	return render_template('entries/index.html')
+# template以下に有るentries/index.htmlを返して(レンダリングして)あげると言う設定
+# パスにtemplateを設定しないのは、flaskではtemplatesフォルダ以下に自動でhtmlファイルがある(ルール)と認識するから。
