236b25582f4986f698401c0d63877927943f9442
Shuji Kato <shuwants@gmail.com>
2021-04-11 17:31:33 +0900

    Create List function - Read of CRUD

--- flask_blog/models/entries.py
+++ flask_blog/models/entries.py
@@ -1,18 +1,18 @@
 from flask_blog import db
 from datetime import datetime
 
-class Entry(db.Model): # 実際のモデルの定義。モデル(今回モデル名はEntry)はclassとして定義。
-    __tablename__ = 'entries' # 実際のデータベースに格納されるテーブル名。実際はEntryモデルを経由して操作。
+
+class Entry(db.Model):  # 実際のモデルの定義。モデル(今回モデル名はEntry)はclassとして定義。
+    __tablename__ = 'entries'  # 実際のデータベースに格納されるテーブル名。実際はEntryモデルを経由して操作。
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), unique=True)
     text = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
 
-    def __init__(self, title=None, text=None): # モデルが作成された時の標準の動作。
+    def __init__(self, title=None, text=None):  # モデルが作成された時の標準の動作。
         self.title = title
         self.text = text
         self.created_at = datetime.utcnow()
 
-    def __repr__(self): # 無くても良いがあると嬉しい、実際に記事モデルが参照された時のコンソールでの出力形式について
+    def __repr__(self):  # 無くても良いがあると嬉しい、実際に記事モデルが参照された時のコンソールでの出力形式について
         return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
-
--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,12 +1,16 @@
-from flask import request, redirect, url_for, render_template, flash, session # 後使用も含め必要パッケージインストール
-from flask_blog import app # __init__.pyで作成したappをインポート
+from flask import request, redirect, url_for, render_template, flash, session  # 後使用も含め必要パッケージインストール
+from flask_blog import app  # __init__.pyで作成したappをインポート
 from flask_blog import db
+from flask_blog.models.entries import Entry
+
 
 @app.route('/')
 def show_entries():
-	if not session.get('logged_in'):
-		return redirect(url_for('login'))
-	return render_template('entries/index.html')
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('entries/index.html', entries=entries)
+
 # template以下に有るentries/index.htmlを返して(レンダリングして)あげると言う設定
 # パスにtemplateを設定しないのは、flaskではtemplatesフォルダ以下に自動でhtmlファイルがある(ルール)と認識するから。
 
@@ -14,12 +18,6 @@ def show_entries():
 # 分けた事で、今後記事の作成・一覧など記事に関連するビューは、entries.pyのみアップデートすればよくなった
 # また、モデルごとにビューを用意することでモデルとビューが１対１の紐付けになり、わかりやすくなった。
 
-# new_entryビューを追加 p856
-@app.route('/entries/new', methods=['GET'])
-def new_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/new.html')
 
 # add_entryビューを作成、投稿内容を受信してデータベースに保存、そしてadd_entryビューを追加
 @app.route('/entries', methods=['POST'])  # ログインフォーム作成時と同様、データ送信にはPOSTメソッド指定。
@@ -34,3 +32,10 @@ def add_entry():
     db.session.commit()
     flash('新しく記事が作成されました')
     return redirect(url_for('show_entries'))
+
+
+@app.route('/entries/new', methods=['GET'])  # new_entryビューを追加 p856
+def new_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/new.html')
